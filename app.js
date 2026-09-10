0aad459aac29d1caa83917d8b570f7d7232d0143
Steve Benton <steve@bentonemail.uk>
2026-09-10 08:25:21 +0100

    add routes in app.js for each html page

--- app.js
+++ app.js
@@ -1,9 +1,32 @@
+const path = require("path");
+
 const express = require("express");
 
 const app = express();
 
 app.get("/", function (req, res) {
-  res.send("<h1>Hello World! - from my new nodeJS server!</h1>");
+  const htmlFilePath = path.join(__dirname, "views", "index.html");
+  res.sendFile(htmlFilePath);
 }); // localhost:3000/
+
+app.get("/restaurants", function (req, res) {
+  const htmlFilePath = path.join(__dirname, "views", "restaurants.html");
+  res.sendFile(htmlFilePath);
+}); // localhost:3000/restaurants
+
+app.get("/confirm", function (req, res) {
+  const htmlFilePath = path.join(__dirname, "views", "confirm.html");
+  res.sendFile(htmlFilePath);
+}); // localhost:3000/confirm
+
+app.get("/about", function (req, res) {
+  const htmlFilePath = path.join(__dirname, "views", "about.html");
+  res.sendFile(htmlFilePath);
+}); // localhost:3000/about
+
+app.get("/recommend", function (req, res) {
+  const htmlFilePath = path.join(__dirname, "views", "recommend.html");
+  res.sendFile(htmlFilePath);
+}); // localhost:3000/recommend
 
 app.listen(3000);
