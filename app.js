dc745bfdc484c924359e91942140d58c77713d23
Steve Benton <steve@bentonemail.uk>
2026-09-10 11:56:11 +0100

    add post route to accept data from form and store in json file

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const path = require("path");
+const fs = require("fs");
 
 const express = require("express");
 
 const app = express();
 
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", function (req, res) {
   const htmlFilePath = path.join(__dirname, "views", "index.html");
@@ -30,5 +32,18 @@ app.get("/recommend", function (req, res) {
   const htmlFilePath = path.join(__dirname, "views", "recommend.html");
   res.sendFile(htmlFilePath);
 }); // localhost:3000/recommend
+
+app.post("/recommend", function (req, res) {
+  const restaurant = req.body;
+  const filePath = path.join(__dirname, "data", "restaurants.json");
+  const fileData = fs.readFileSync(filePath);
+  const storedRestaurants = JSON.parse(fileData);
+
+  storedRestaurants.push(restaurant);
+
+  fs.writeFileSync(filePath, JSON.stringify(storedRestaurants));
+
+  res.redirect("/confirm");
+});
 
 app.listen(3000);
