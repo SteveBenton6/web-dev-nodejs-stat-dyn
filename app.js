eb264e255b0152848db0989035382445b0ca3cf4
Steve Benton <steve@bentonemail.uk>
2026-09-14 16:14:42 +0100

    add new utils folder and initial js code with functions to read and save to the JSON file

--- app.js
+++ app.js
@@ -57,13 +57,11 @@ app.get("/recommend", function (req, res) {
 app.post("/recommend", function (req, res) {
   const restaurant = req.body;
   restaurant.id = uuid.v4();
-  const filePath = path.join(__dirname, "data", "restaurants.json");
-  const fileData = fs.readFileSync(filePath);
-  const storedRestaurants = JSON.parse(fileData);
+  const restaurants = getStoredRestaurants();
 
-  storedRestaurants.push(restaurant);
+  restaurants.push(restaurant);
 
-  fs.writeFileSync(filePath, JSON.stringify(storedRestaurants));
+  storeRestaurants(restaurants);
 
   res.redirect("/confirm");
 });
