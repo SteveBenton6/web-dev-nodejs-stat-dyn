0ea0e18d1b547f9df4b649117a4f1f342656ad6e
Steve Benton <steve@bentonemail.uk>
2026-09-15 16:00:31 +0100

    add import and export to get restaurant-data functions working

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const fs = require("fs");
 const express = require("express");
 const uuid = require("uuid");
 
+const resData = require("./utils/restaurant-data");
+
 const app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
@@ -16,9 +18,7 @@ app.get("/", function (req, res) {
 }); // localhost:3000/
 
 app.get("/restaurants", function (req, res) {
-  const filePath = path.join(__dirname, "data", "restaurants.json");
-  const fileData = fs.readFileSync(filePath);
-  const storedRestaurants = JSON.parse(fileData);
+  const storedRestaurants = resData.getStoredRestaurants();
 
   res.render("restaurants", {
     numberOfRestaurants: storedRestaurants.length,
@@ -28,10 +28,7 @@ app.get("/restaurants", function (req, res) {
 
 app.get("/restaurants/:id", function (req, res) {
   const restaurantId = req.params.id;
-  const filePath = path.join(__dirname, "data", "restaurants.json");
-
-  const fileData = fs.readFileSync(filePath);
-  const storedRestaurants = JSON.parse(fileData);
+  const storedRestaurants = resData.getStoredRestaurants();
 
   for (const restaurant of storedRestaurants) {
     if (restaurant.id === restaurantId) {
@@ -57,11 +54,11 @@ app.get("/recommend", function (req, res) {
 app.post("/recommend", function (req, res) {
   const restaurant = req.body;
   restaurant.id = uuid.v4();
-  const restaurants = getStoredRestaurants();
+  const restaurants = resData.getStoredRestaurants();
 
   restaurants.push(restaurant);
 
-  storeRestaurants(restaurants);
+  resData.storeRestaurants(restaurants);
 
   res.redirect("/confirm");
 });
--- utils/restaurant-data.js
+++ utils/restaurant-data.js
@@ -1,4 +1,7 @@
-const filePath = path.join(__dirname, "data", "restaurants.json");
+const path = require("path");
+const fs = require("fs");
+
+const filePath = path.join(__dirname, "..", "data", "restaurants.json");
 
 function getStoredRestaurants() {
   const fileData = fs.readFileSync(filePath);
@@ -9,3 +12,8 @@ function getStoredRestaurants() {
 function storeRestaurants(storableRestaurants) {
   fs.writeFileSync(filePath, JSON.stringify(storableRestaurants));
 }
+
+module.exports = {
+  getStoredRestaurants: getStoredRestaurants,
+  storeRestaurants: storeRestaurants,
+};
